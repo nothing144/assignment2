1a7c45d8436f76622abbe94da0e29a0d3a62a2fc
Harsh pandey <thepandey144@gmail.com>
2024-11-11 21:08:24 +0530

    Update question2.java

--- question2.java
+++ question2.java
@@ -4,7 +4,7 @@ public class question2 {
 	public static void main(String[] args) {
 		System.out.println("enter distance in km ");
 		Scanner sc = new Scanner(System.in);
-		long km = sc.nextLong();
+		double km = sc.nextLong();
 		double meter = km * 1000d;
 		double feet = km * 3280.8399d;
 		double inch = km * 39370.07087d;
