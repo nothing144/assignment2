bc6e42528067e4746261a62b284591fc753975a4
Harsh pandey <thepandey144@gmail.com>
2024-11-12 20:01:56 +0530

    Update question6.java

--- question6.java
+++ question6.java
@@ -4,8 +4,9 @@ public class question6 {
 public static void main(String[] args) {
 	System.out.println("input time");
 	Scanner sc = new Scanner(System.in);
-	int t = sc.nextInt();
-	float d = (0.5f)*32.174f*t*t; 
+	double t = sc.nextDouble();
+	double time = Math.pow(t, 2);
+	double d = (0.5d)*32.174d*t; 
 System.out.println("Distance travelled " + d);	
 sc.close();
  }
