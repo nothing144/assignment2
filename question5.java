24bafc9c8bae3c4490c8aa10799d3262ff8f4472
Harsh pandey <thepandey144@gmail.com>
2024-11-12 19:59:37 +0530

    Update question5.java

--- question5.java
+++ question5.java
@@ -4,12 +4,13 @@ public class question5 {
 	public static void main(String[] args) {
 		System.out.println("enter radius");
 		Scanner sc = new Scanner(System.in);
-		int radius = sc.nextInt();
-		float area = 3 * 3.14f * radius * radius;
-		float volume = (2.0f/3.14f)*radius * radius * radius;
-		System.out.println("area of hemisphere " + area);
-		System.out.println("volume of hemisphere " + volume);
-		sc.close();
+		double radius = sc.nextDouble();
+		double r = Math.pow(radius, 2);
+		double pi = Math.PI;
+		double area = 3*pi*r;
+		double r2 = Math.pow(radius, 3);
+		double volume = (2.0/3)*pi*r2;
+		System.out.println("the volume of hemisphere is " + volume + "\nthe area of hemisphere is "+ area);
 	}
 
 }
