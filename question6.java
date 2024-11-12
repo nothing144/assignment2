331531bf1a4d0445ad120a2b99a0c6c67e605047
Harsh pandey <thepandey144@gmail.com>
2024-11-12 20:03:49 +0530

    Update question6.java

--- question6.java
+++ question6.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	double t = sc.nextDouble();
 	double time = Math.pow(t, 2);
-	double d = (0.5d)*32.174d*t; 
+	double d = (0.5d)*32.174d*time; 
 System.out.println("Distance travelled " + d);	
 sc.close();
  }
