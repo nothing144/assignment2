1415b1734df12119fcc52b0e9f469e47e9809243
Harsh pandey <thepandey144@gmail.com>
2024-11-20 00:14:34 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,10 @@
+public class Q1{ 
+public static void main(String args[]) { 
+int dividand = Integer.parseInt(args[0]);
+int divisor = Integer.parseInt(args[1]);
+int quotient = dividand/divisor;
+int remainder = dividand%divisor;
+System.out.println(quotient);
+System.out.println(remainder);
+  }
+}
