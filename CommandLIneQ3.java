763a05dd2a6f16e459a5211d92d1ef6789667ac9
Harsh pandey <thepandey144@gmail.com>
2024-11-29 20:03:38 +0530

    Create CommandLIneQ3.java

---
+++ CommandLIneQ3.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class DiceSum {
+    public static void main(String[] args) {
+        // Create an instance of Random
+        Random random = new Random();
+        
+        // Generate two random integers between 1 and 6
+        int die1 = random.nextInt(6) + 1;
+        int die2 = random.nextInt(6) + 1;
+        
+        // Calculate the sum
+        int sum = die1 + die2;
+        
+        // Print the results
+        System.out.println("Roll of die 1: " + die1);
+        System.out.println("Roll of die 2: " + die2);
+        System.out.println("Sum of the two dice rolls: " + sum);
+    }
+}
